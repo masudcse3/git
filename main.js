d2daa1e72648403ca808c350b5e465583cb6f885
masud <masud_cse3@yahoo.com>
2022-12-19 13:01:56 +0600

    basic setup is done

---
+++ main.js
@@ -0,0 +1,6 @@
+/** @format */
+const app = document.getElementById("app");
+const tag = document.createElement("h4");
+tag.innerText = "Git is Awesome and Github is Beautiful !!!";
+
+app.appendChild(tag);
